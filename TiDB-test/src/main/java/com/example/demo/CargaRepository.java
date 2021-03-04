84bd44af88aaec9db9a0da1677ee5d4b91c2af8a
r <r@r>
2021-03-04 15:41:25 +0100

    Repository clases complete

---
+++ TiDB-test/src/main/java/com/example/demo/CargaRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CargaRepository extends CrudRepository<Carga,Integer>{
+
+}
---
+++ TiDB-test/src/main/java/com/example/demo/PropietarioRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PropietarioRepository extends CrudRepository<Propietario,Integer>{
+
+}
